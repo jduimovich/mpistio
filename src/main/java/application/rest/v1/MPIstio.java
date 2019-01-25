c1c23f6ee9736c2a01cff929fdac281631c746a9
Microclimate User <jduimovich@gmail.com>
2019-01-25 03:11:28 +0000

    demo 1.0

--- src/main/java/application/rest/v1/MPIstio.java
+++ src/main/java/application/rest/v1/MPIstio.java
@@ -1,24 +1,25 @@
-package application.rest;
+package application.rest.v1;
 
 import java.io.InputStream;
 
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces; 
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response; 
-
-import java.util.List; 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Context;
 import java.util.ArrayList;
-  
+ 
+import javax.ws.rs.core.Context; 
 import javax.ws.rs.core.UriInfo;
 
 
-@Path("/")
-public class RootEndpoint {
+@Path("/mpistio")
+public class MPIstio {
+
     static String kv(String k, String v) { 
        return "\""
         + k
@@ -26,19 +27,30 @@ public class RootEndpoint {
         + v 
         + "\"";
     }
-    
-    @GET
-    @Path("frontpage")
+
+    // default path will be / under mpistio
+    @GET 
     @Produces({ MediaType.TEXT_HTML })
-    public InputStream getFrontPage() {
+    public InputStream getmpistio() {
         try {
-            return this.getClass().getResourceAsStream("/index.html");
+            return this.getClass().getResourceAsStream("/root.html");
         } catch (Exception e) {
             throw new RuntimeException("Exception returning index.html", e);
         }
     }
 
-    // /get 
+    // mpistio/example 
+    @GET
+    @Path("example")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response example() {
+        List<String> list = new ArrayList<>();
+        //return a simple list of strings
+        list.add("Sample Microservice");
+        return Response.ok(list.toString()).build();
+    }
+
+    // mpistio/get 
     @GET
     @Path("get")
     @Produces(MediaType.APPLICATION_JSON)
@@ -48,16 +60,16 @@ public class RootEndpoint {
         sb.append ("{");
         sb.append (kv("time", Long.toString(t)));
         sb.append (",");
-        sb.append (kv("path", "/get"));
+        sb.append (kv("path", "/mpistio/get"));
         sb.append (",");
         sb.append (kv("version", "1.0"));
         sb.append ("}");
         return Response.ok(sb.toString()).build();
     }
 
-    // /all 
+    // mpistio/all 
     @GET
-    @Path("all")
+    @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public Response apiAll(@Context HttpHeaders headers) { 
         String response = "{\n";
@@ -69,15 +81,7 @@ public class RootEndpoint {
         response += "\n}"; 
         return Response.ok(response).build();
     }
+ 
 
 
-    @GET
-    @Produces({ MediaType.TEXT_HTML })
-    public InputStream getIndex() {
-        try {
-            return this.getClass().getResourceAsStream("/root.html");
-        } catch (Exception e) {
-            throw new RuntimeException("Exception returning index.html", e);
-        }
-    }
 }
